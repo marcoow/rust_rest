8e4d83a8ed33714a17670419eb30ec1edbcb1d06
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2023-01-13 12:37:10 +0100

    read user from DB

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
+use axum::{extract::State, http::StatusCode, routing::get, Json, Router};
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
 use serde::Serialize;
 use std::net::SocketAddr;
+use tokio_postgres::NoTls;
 use tracing::{debug, info, Level};
 use tracing_subscriber::FmtSubscriber;
 
@@ -12,8 +15,17 @@ async fn main() {
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
+    let manager = PostgresConnectionManager::new_from_stringlike(
+        "postgresql://rust_rest:rust_rest@localhost/rust_rest",
+        NoTls,
+    )
+    .unwrap();
+    let pool = Pool::builder().build(manager).await.unwrap();
+
     // build our application with a route
-    let app = Router::new().route("/users", get(get_users));
+    let app = Router::new()
+        .route("/users", get(get_users))
+        .with_state(pool);
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
@@ -25,23 +37,39 @@ async fn main() {
         .unwrap();
 }
 
-async fn get_users() -> impl IntoResponse {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: "Eine Person".to_string(),
-    };
+type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
+
+#[derive(Serialize, Debug)]
+struct User {
+    id: i32,
+    username: String,
+}
+
+async fn get_users(
+    State(pool): State<ConnectionPool>,
+) -> Result<Json<Vec<User>>, (StatusCode, String)> {
+    let conn = pool.get().await.map_err(internal_error)?;
+
+    let row = conn
+        .query_one("select id, username from users where id = $1", &[&1])
+        .await
+        .map_err(internal_error)?;
+
+    let id = row.get(0);
+    let username = row.get(1);
+
+    let user = User { id, username };
 
     info!("responding with {:?}", user);
 
     // this will be converted into a JSON response
     // with a status code of `201 Created`
-    (StatusCode::OK, Json([user]))
+    Ok(Json(vec![user]))
 }
 
-// the output to our `create_user` handler
-#[derive(Serialize, Debug)]
-struct User {
-    id: u64,
-    username: String,
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
