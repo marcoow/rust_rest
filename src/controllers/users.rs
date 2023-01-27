8d2b98c9acb9030a883f83a80e9a0d6dac1ed8c7
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2023-01-27 14:50:19 +0100

    give each test its own database

--- src/controllers/users.rs
+++ src/controllers/users.rs
@@ -98,8 +98,11 @@ mod tests {
     use axum::{body::Body, http::Request, routing::get, Router};
     use bb8::Pool;
     use bb8_postgres::PostgresConnectionManager;
+    use core::str::FromStr;
     use dotenv_codegen::dotenv;
-    use tokio_postgres::NoTls;
+    use rand::distributions::Alphanumeric;
+    use rand::{thread_rng, Rng};
+    use tokio_postgres::{config::Config, NoTls};
     use tower::ServiceExt;
 
     type UsersList = Vec<User>;
@@ -107,8 +110,35 @@ mod tests {
     #[tokio::test]
     async fn test_get_users() {
         let db_url = dotenv!("DATABASE_URL");
+        let config = Config::from_str(db_url).unwrap();
+        let db_name = config.get_dbname().unwrap();
 
-        let manager = PostgresConnectionManager::new_from_stringlike(db_url, NoTls).unwrap();
+        let (client, connection) = tokio_postgres::connect(db_url, NoTls).await.unwrap();
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+
+        let test_db_suffix: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(30)
+            .map(char::from)
+            .collect();
+        let test_db_name = format!("{}_{}", db_name, test_db_suffix).to_lowercase();
+        let res = client
+            .execute(
+                &format!("create database {} template {}", test_db_name, db_name),
+                &[],
+            )
+            .await
+            .unwrap();
+        println!("{:?}", res);
+
+        let mut test_db_config = config.clone();
+        test_db_config.dbname(&test_db_name);
+        println!("{:?}", test_db_config);
+        let manager = PostgresConnectionManager::new(test_db_config, NoTls);
         let pool = Pool::builder().build(manager).await.unwrap();
 
         let app = Router::new()
