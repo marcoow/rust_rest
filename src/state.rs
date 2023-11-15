2287776a621598de51cb892e256c17bdd6fc0990
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2023-11-15 15:19:06 +0100

    add sqlx pool to app state

--- src/state.rs
+++ src/state.rs
@@ -4,18 +4,23 @@ use bb8_postgres::PostgresConnectionManager;
 use dotenvy::dotenv;
 use std::env;
 use tokio_postgres::NoTls;
+use sqlx::postgres::{PgPoolOptions, PgPool};
 
 #[derive(Clone)]
 pub struct AppState {
     pub db_pool: ConnectionPool,
+    pub sqlx_pool: PgPool,
 }
 
 pub async fn app_state() -> AppState {
     dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("No DATABASE_URL set – cannot start server!");
 
+    let sqlx_pool = PgPoolOptions::new()
+        .connect(db_url.as_str()).await.expect("Could not connect to database!");
+
     let manager = PostgresConnectionManager::new_from_stringlike(&db_url, NoTls).unwrap();
     let pool = Pool::builder().build(manager).await.unwrap();
 
-    AppState { db_pool: pool }
+    AppState { db_pool: pool, sqlx_pool }
 }
