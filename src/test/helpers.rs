99ff3012c600ef867eb6ab9317436c4facd74d92
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2023-11-15 12:37:47 +0100

    rename var

--- src/test/helpers.rs
+++ src/test/helpers.rs
@@ -6,13 +6,13 @@ use bb8_postgres::PostgresConnectionManager;
 use tokio_postgres::NoTls;
 
 pub async fn setup() -> TestContext {
-    let test_db_config = prepare_db().await;
-    let manager = PostgresConnectionManager::new(test_db_config.clone(), NoTls);
+    let db_config = prepare_db().await;
+    let manager = PostgresConnectionManager::new(db_config.clone(), NoTls);
     let pool = Pool::builder().build(manager).await.unwrap();
 
     let app = routes(AppState {
         db_pool: pool.clone(),
     });
 
-    build_test_context(app, pool, test_db_config)
+    build_test_context(app, pool, db_config)
 }
