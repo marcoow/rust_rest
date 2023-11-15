fa21ee4f7eedfaccc465908d40b1572323781a1b
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2023-11-15 12:21:01 +0100

    remove tracing from binaries

--- src/bin/generate.rs
+++ src/bin/generate.rs
@@ -1,14 +1,6 @@
 use axum_on_rails::cli::generate::cli;
-use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() {
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
-        .finish();
-
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-
     cli().await;
 }
