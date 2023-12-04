8cf0e6696fc56c045bb1be3a39cd55d950413eba
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2023-12-04 14:10:07 +0100

    remove unused elements from app state

--- web/src/state.rs
+++ web/src/state.rs
@@ -1,23 +1,16 @@
-use axum_on_rails::Environment;
 use rust_rest_config::Config;
 use sqlx::postgres::{PgPool, PgPoolOptions};
 
 #[derive(Clone)]
 pub struct AppState {
     pub db_pool: PgPool,
-    pub config: Config,
-    pub env: Environment,
 }
 
-pub async fn app_state(config: Config, env: Environment) -> AppState {
+pub async fn app_state(config: Config) -> AppState {
     let db_pool = PgPoolOptions::new()
         .connect(config.database.url.as_str())
         .await
         .expect("Could not connect to database!");
 
-    AppState {
-        db_pool,
-        config,
-        env,
-    }
+    AppState { db_pool }
 }
